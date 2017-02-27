1394369e45ba6a4bf1bef0aa25087f4374860a6e
ngrotz <ngrotz@mines.edu>
2017-02-27 15:47:49 -0700

    Our solution, but with errors, bc test are still failing

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -6,53 +6,99 @@ public class IntBoard {
 	private BoardCell[][] board;
 	private Map<BoardCell, Set <BoardCell>> adjacencyMap;
 	private Set<BoardCell> targets;
+	private Set<BoardCell> visitedList;
 
 	
 	
 	public IntBoard() {
 		super();
 		adjacencyMap = new HashMap<BoardCell, Set<BoardCell>>();
+		visitedList = new HashSet<BoardCell>();
 		targets = new HashSet<BoardCell>();
-		board = new BoardCell[4][4];
+		//board = new BoardCell[4][4];
+		for (int i = 0; i < 4; i++){
+			for (int j = 0; j < 4; j++){
+				board[i][j] = new BoardCell(i,j);
+			}
+		}
 	}
 	
 
 	public void calcAdjacencies(BoardCell cell){
-		if((cell.getRow() >= 0) && (cell.getRow() < 4)){
-			for (int i = cell.getRow(); i <=1; i++){
-				for (int j = cell.getColumn(); j <=1; j++){
-					if (i != null && j != null){
-						//set.add something
-					}
-				}
-			}
-			if((cell.getColumn() > 0) && (cell.getColumn() < 4)){
+		Set<BoardCell> temp = null;
+		if((cell.getRow() > 0) && (cell.getRow() < 4)){
+			if((cell.getColumn() >= 0) && (cell.getColumn() < 4)){
+				temp.add(board[cell.getRow() - 1][cell.getColumn()]);
+				temp.add(board[cell.getRow() + 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() - 1]);
+				temp.add(board[cell.getRow()][cell.getColumn() + 1]);						
 			}
 			if(cell.getColumn() == 0){
+				temp.add(board[cell.getRow() - 1][cell.getColumn()]);
+				temp.add(board[cell.getRow() + 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() + 1]);	
 			}
 			if(cell.getColumn() == 3){
+				temp.add(board[cell.getRow() - 1][cell.getColumn()]);
+				temp.add(board[cell.getRow() + 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() - 1]);
 			}
 		}
 		else if(cell.getRow() == 0){
 			if((cell.getColumn() > 0) && (cell.getColumn() < 3)){
+				temp.add(board[cell.getRow() + 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() - 1]);
+				temp.add(board[cell.getRow()][cell.getColumn() + 1]);	
 			}
 			if(cell.getColumn() == 0){
+				temp.add(board[cell.getRow() + 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() + 1]);	
 			}
 			if(cell.getColumn() == 3){
+				temp.add(board[cell.getRow() + 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() - 1]);	
 			}
 		}
 		else if(cell.getRow() == 3){
 			if((cell.getColumn() > 0) && (cell.getColumn() < 3)){
+				temp.add(board[cell.getRow() - 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() - 1]);
+				temp.add(board[cell.getRow()][cell.getColumn() + 1]);
 			}
 			if(cell.getColumn() == 0){
+				temp.add(board[cell.getRow() - 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() + 1]);	
 			}
 			if(cell.getColumn() == 3){
+				temp.add(board[cell.getRow() - 1][cell.getColumn()]);
+				temp.add(board[cell.getRow()][cell.getColumn() - 1]);
 			}
 		}
+		adjacencyMap.put(cell, temp);
+}
+		
+	
+	public void calcTargets(BoardCell startCell,int pathLength){
+		visitedList.add(startCell);
+		findAllTargets(startCell, pathLength);
 		
 	}
 	
-	public void calcTargets(BoardCell startCell,int pathLength){	
+	public void findAllTargets(BoardCell thisCell, int numSteps){
+		Set<BoardCell> list = getAdjList(thisCell);
+		for(BoardCell a : list){
+			if(visitedList.contains(a)){
+				return;
+			}
+			visitedList.add(a);
+			if(numSteps == 1){
+				 targets.add(a);
+				}
+			else{
+				findAllTargets(a, numSteps - 1);
+			}
+			visitedList.remove(a);
+		}
 	}
 	
 	public Set<BoardCell> getTargets(){
@@ -60,11 +106,11 @@ public class IntBoard {
 	}
 	
 	public Set<BoardCell> getAdjList(BoardCell test){
-		return null;
+		return adjacencyMap.get(test);
 	}
 	
 	public BoardCell getCell(int row, int col){
-		return null;
+		return board[row][col];
 	}
 
 }

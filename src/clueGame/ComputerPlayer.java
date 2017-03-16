cf281d3e90489a150012818bae595c913a2e1a1e
Madison Klein <madisonklein@mines.edu>
2017-03-16 12:15:10 -0600

    started people and createDeck tests

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -1,10 +1,11 @@
 package clueGame;
 
+import java.awt.Color;
 import java.util.Set;
 
 public class ComputerPlayer extends Player{
 
-	public ComputerPlayer(String playerName, int row, int column, String color) {
+	public ComputerPlayer(String playerName, int row, int column, Color color) {
 		super(playerName, row, column, color);
 
 	}
--- src/tests/gameSetupTests.java
+++ src/tests/gameSetupTests.java
@@ -3,17 +3,23 @@ package tests;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import clueGame.BadConfigFormatException;
 import clueGame.Board;
+import clueGame.Card;
+import clueGame.ComputerPlayer;
+import clueGame.HumanPlayer;
+import clueGame.Player;
 
 public class gameSetupTests {
 
 	private static Board board;
 	private static final int NUM_PEOPLE = 6;
+	private static final int DECK_SIZE = 21;
 	
 	@Before
 	public void setUpClass() throws IOException, BadConfigFormatException  {
@@ -25,12 +31,16 @@ public class gameSetupTests {
 	
 	@Test
 	public void testPeople() {
+		ArrayList<ComputerPlayer> people = board.getPeople();
+		HumanPlayer human = board.getHumanPlayer();
+		//assertTrue(human.equals(new Human("Dr. Rader", Color.)))
 		
 	}
 	
 	@Test
 	public void testCreateDeck() {
-		
+		ArrayList<Card> deck = board.getDeck();
+		assertEquals(deck.size(), DECK_SIZE);
 	}
 	
 	@Test

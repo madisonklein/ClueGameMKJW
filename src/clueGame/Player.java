0a16d3e6e848813e165101c7c8b1ff358826f108
Madison Klein <madisonklein@mines.edu>
2017-03-16 11:32:03 -0600

    Creating player class and setUp tests

---
+++ src/clueGame/Player.java
@@ -0,0 +1,17 @@
+package clueGame;
+
+import java.awt.Color;
+
+public class Player {
+	
+	private String playerName;
+	private int row;
+	private int column;
+	private Color color;
+	
+	public Player(String playerName, int row, int column, String color) {
+		this.playerName = playerName;
+		this.row = row;
+	}
+
+}
---
+++ src/tests/gameSetupTests.java
@@ -0,0 +1,41 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import clueGame.BadConfigFormatException;
+import clueGame.Board;
+
+public class gameSetupTests {
+
+	private static Board board;
+	private static final int NUM_PEOPLE = 6;
+	
+	@Before
+	public void setUpClass() throws IOException, BadConfigFormatException  {
+		board = Board.getInstance();
+		board.setConfigFiles("Layout.csv", "Legend.txt");
+		// board.initalize() loads config files, creates the deck, and deals the cards
+		board.initialize();
+	}
+	
+	@Test
+	public void testPeople() {
+		Map<String, >
+	}
+	
+	@Test
+	public void testCreateDeck() {
+		
+	}
+	
+	@Test
+	public void testDealCard() {
+		
+	}
+
+}
